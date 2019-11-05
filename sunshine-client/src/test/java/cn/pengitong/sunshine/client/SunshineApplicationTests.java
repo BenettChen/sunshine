23c42c0d100fd3f3420a37dfef897287cb73c9ad
“peng.chen1” <peng.chen1@thoughtworks.com>
2019-11-05 17:39:59 +0800

    完善pom文件依赖

--- sunshine-client/src/test/java/cn/pengitong/sunshine/client/SunshineApplicationTests.java
+++ sunshine-client/src/test/java/cn/pengitong/sunshine/client/SunshineApplicationTests.java
@@ -1,16 +1,26 @@
 package cn.pengitong.sunshine.client;
 
+import cn.pengitong.sunshine.persist.dao.bean.ArticleContentDO;
+import cn.pengitong.sunshine.persist.dao.mapper.ArticleContentMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SunshineApplicationTests {
 
+    @Resource
+    private ArticleContentMapper articleContentMapper;
+
     @Test
     public void contextLoads() {
+        List<ArticleContentDO> articleContentDOS = articleContentMapper.queryArticleContent();
+        System.out.println(articleContentDOS);
     }
 
 }
