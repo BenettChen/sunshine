b5b44ea777627e38edbb077cdf4bf29d18e2c23b
peng.chen1 <peng.chen1@thoughtworks.com>
2020-02-06 16:15:06 +0800

    修改提交记录

--- sunshine-client/src/main/java/cn/pengitong/sunshine/SpringBootStartApplication.java
+++ sunshine-client/src/main/java/cn/pengitong/sunshine/SpringBootStartApplication.java
@@ -33,8 +33,8 @@ public class SpringBootStartApplication extends SpringBootServletInitializer {
         System.out.println("添加第四个测试");
     }
 
-    public void addTestFuncation5() {
-        System.out.println("添加第五个测试");
+    public void addTestFuncation6() {
+        System.out.println("添加第六个测试");
     }
 
     public void addTestFuncation7() {
